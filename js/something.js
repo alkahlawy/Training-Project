e3a58d9f168c86de11e88b6522af23ddabe19c5f
Alkahlawy <mohamedalkahlawy22@gmail.com>
2023-07-08 05:15:00 +0300

    add files

---
+++ js/something.js
@@ -0,0 +1,16 @@
+const text = document.querySelector(".sec-text");
+
+const textLoad = ()=> {
+    setTimeout(() => {
+        text.textContent = "Developer"
+    }, 0);
+    setTimeout(() => {
+        text.textContent = "Freelancer"
+    }, 4000);
+    setTimeout(() => {
+        text.textContent = "Designer"
+    }, 8000);
+}
+
+textLoad();
+setInterval(textLoad,12000);
